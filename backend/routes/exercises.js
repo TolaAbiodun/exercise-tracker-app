17a3cf812bbf87690c45d4862c1b41c66f450986
TolaAbiodun <adetolaabiodun@outlook.com>
2021-10-12 20:31:53 +0100

    added CRUD operations for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,5 +1,6 @@
 // Import express router and require user model
 const router = require('express').Router();
+const { response } = require('express');
 let Exercise = require('../models/exercise_model');
 
 // First End point for HTTP get requests
@@ -29,6 +30,34 @@ router.route('/add').post((req,res)=>{
     newExercise.save()
         .then(()=> res.json('New exercise added!'))
         .catch(err => res.status(400).json('Error:' + err));
+});
+
+// Create UD operations for exercises
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err=>res.status(400).json('Error:' + err));
+    });
+
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted'))
+        .catch(err => res.status(400).json('Error:' + err))
+});
+
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+            .then(() => res.json('Updated Exercise!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
